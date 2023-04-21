0b5173a6a51c1a0e639373d281fc87da7f47162f
hoangnb2501 <hoanguyen25011@gmail.com>
2023-04-21 08:28:55 +0700

    Add Header Section and Fix render root element with React v18

--- frontend_react/src/constants/image.js
+++ frontend_react/src/constants/image.js
@@ -3,6 +3,7 @@ import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
+import csharp from '../assets/csharp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
 import flutter from '../assets/flutter.png';
@@ -11,6 +12,7 @@ import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import mu5 from '../assets/mu5.png';
+import dotnet from '../assets/dotnet.png';
 import node from '../assets/node.png';
 import python from '../assets/python.png';
 import react from '../assets/react.png';
@@ -18,6 +20,11 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
+import mysql from '../assets/mysql.png';
+import express from '../assets/express.png';
+import nest from '../assets/nest.png';
+import sqlserver from '../assets/microsoft-sql-server.png';
+import mongodb from '../assets/mongodb.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -25,6 +32,7 @@ import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
 import profile from '../assets/profile.png';
+import profile_2 from '../assets/profile-2.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 import logo_2 from '../assets/logo-2.png';
@@ -42,6 +50,7 @@ export default {
   mobile,
   api,
   cpp,
+  csharp,
   css,
   figma,
   flutter,
@@ -51,17 +60,24 @@ export default {
   javascript,
   mu5,
   node,
+  dotnet,
   python,
   react,
   redux,
   sass,
   typescript,
   vue,
+  express,
+  nest,
+  mysql,
+  sqlserver,
+  mongodb,
   about01,
   about02,
   about03,
   about04,
   profile,
+  profile_2,
   circle,
   logo,
   logo_2,
