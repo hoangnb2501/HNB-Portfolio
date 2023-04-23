6db461f4e4a15d2c31084d1e6c642abe459fffa1
hoangnb2501 <hoanguyen25011@gmail.com>
2023-04-23 16:36:12 +0700

    Add Skills section, MotionWrap for each of section

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { easeInOut, motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -72,4 +72,8 @@ const Header = () => {
   );
 };
 
-export default AppWrap(Header, 'home');
+export default AppWrap(
+  MotionWrap(Header, 'app_home'),
+  'home',
+  'app__primarybg',
+);
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -3,7 +3,7 @@ import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
 import './Work.scss';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
 const Work = () => {
@@ -116,4 +116,4 @@ const Work = () => {
   );
 };
 
-export default AppWrap(Work, 'work');
+export default AppWrap(MotionWrap(Work, 'app__work'), 'work', 'app__primarybg');
